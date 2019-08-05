e05dfceb55a453993f7ef8f6ecc3becd7505753b
Denis Girko <luza.mbox@gmail.com>
2019-08-05 14:29:49 +0300

    Added Habr demo project (#12)

---
+++ examples/traffic-lights-demo/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// возможные состояния светофора
+const (
+	lightRed    = "red"
+	lightYellow = "yellow"
+	lightGreen  = "green"
+)
+
+// структура для хранения состояния светофора
+type trafficLights struct {
+	CurrentLight string `json:"currentLight"`
+}
+
+// экземпляр светофора
+var lights = trafficLights{
+	CurrentLight: lightRed,
+}
+
+func main() {
+	initServer()
+
+	// запуск сервера (блокирующий)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initServer() {
+	// метод для получения текущего состояния светофора
+	http.HandleFunc("/light/get", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := json.Marshal(lights)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(resp)
+	})
+
+	// метод для установки нового состояния светофора
+	http.HandleFunc("/light/set", func(w http.ResponseWriter, r *http.Request) {
+		request, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var newTrafficLights trafficLights
+		if err := json.Unmarshal(request, &newTrafficLights); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := validateRequest(&newTrafficLights); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		lights = newTrafficLights
+	})
+}
+
+func validateRequest(lights *trafficLights) error {
+	if lights.CurrentLight != lightRed &&
+		lights.CurrentLight != lightYellow &&
+		lights.CurrentLight != lightGreen {
+		return fmt.Errorf("incorrect current light: %s", lights.CurrentLight)
+	}
+	return nil
+}
