2749c3a7918e2d7ce35f1651ab5e5279246f8d57
Pavel Vaynerman <pv@etersoft.ru>
2021-02-01 01:36:51 +0300

    [response-transform]: fixed build cmd_runner for windows

--- cmd_runner/cmd_runner_windows.go
+++ cmd_runner/cmd_runner_windows.go
@@ -11,20 +11,44 @@ import (
 )
 
 func CmdRun(scriptPath string, timeout int) error {
-	//by default timeout should be 3s
-	if timeout <= 0 {
-		timeout = 3
-	}
-	cmd := exec.Command(strings.TrimRight(scriptPath, "\n"))
-	cmd.Env = os.Environ()
-
-	stdout, err := cmd.StdoutPipe()
+	output, err := CmdRunWithIO(scriptPath, "", timeout)
 	if err != nil {
 		return err
 	}
 
+	fmt.Println(output)
+	return nil
+}
+
+func CmdRunWithIO(scriptPath string, input string, timeout int) (string, error) {
+	//by default timeout should be 3s
+	if timeout <= 0 {
+		timeout = 3
+	}
+
+	args := strings.Split(strings.TrimRight(scriptPath, "\n"), " ")
+	script := args[0]
+	if len(args) > 1 {
+		args = args[1:]
+	} else {
+		args = args[:0]
+	}
+
+	var output string
+
+	cmd := exec.Command(script, args...)
+	cmd.Env = os.Environ()
+	if len(input) > 0 {
+		cmd.Stdin = strings.NewReader(input)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return output, err
+	}
+
 	if err := cmd.Start(); err != nil {
-		return err
+		return output, err
 	}
 
 	done := make(chan error, 1)
@@ -32,26 +56,26 @@ func CmdRun(scriptPath string, timeout int) error {
 		done <- cmd.Wait()
 	}()
 
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			output += scanner.Text()
+		}
+	}()
+
 	select {
 	case <-time.After(time.Duration(timeout) * time.Second):
 		// Kill process
 		if err := cmd.Process.Kill(); err != nil {
-			return err
+			return output, err
 		}
-		fmt.Printf("Process killed as timeout(%d) reached\n", timeout)
+		log.Printf("Process killed as timeout(%d) reached\n", timeout)
 	case err := <-done:
 		if err != nil {
-			return fmt.Errorf("process finished with error = %v", err)
+			return output, fmt.Errorf("process finished with error = %v", err)
 		}
 		log.Print("Process finished successfully")
 	}
 
-	// Print log
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		m := scanner.Text()
-		fmt.Println(m)
-	}
-
-	return nil
+	return output, nil
 }
